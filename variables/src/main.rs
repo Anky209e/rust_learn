c51120736eda3292371278972742c0586f271202
Ankit Kumar <nasaankit01@gmail.com>
2023-12-06 20:08:07 +0530

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // mut for mutable and i32 is datatype
+    let mut x: i32 = 45;
+    let mut y: i64 = 344;
+    let z: f64 = 4.3;
+    let b:bool = false;
+    let p:&str = "ankit";
+    let q: char = 'a';
+
+
+
+
+    println!(" {x} \n {y} \n {z} \n {b} {p} \n {q}");
+
+}
