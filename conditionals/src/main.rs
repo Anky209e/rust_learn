045fa79fe15886a41d0084ac30f4aadba771660b
Ankit Kumar <nasaankit01@gmail.com>
2023-12-06 20:19:02 +0530

    conditionals

---
+++ conditionals/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    
+    let mut x = 29;
+    let mut y = 29;
+
+    if x < y {
+        println!("{x} is less than {y}");
+    } 
+    else if x == y {
+        println!("{x} is equal to {y}");
+    } 
+    else {
+        println!("No condition macthes");
+    }
+}
