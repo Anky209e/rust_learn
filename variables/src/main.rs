0f7a1f36fcbbfcee9ed08ae4d9c4424e1d65d657
Ankit Kumar <nasaankit01@gmail.com>
2025-11-14 13:40:48 +0530

    added comment

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,13 +3,11 @@ fn main() {
     let mut x: i32 = 45;
     let mut y: i64 = 344;
     let z: f64 = 4.3;
-    let b:bool = false;
-    let p:&str = "ankit";
+    let b: bool = false;
+    let p: &str = "ankit";
     let q: char = 'a';
 
-
-
+    // print
 
     println!(" {x} \n {y} \n {z} \n {b} {p} \n {q}");
-
 }
